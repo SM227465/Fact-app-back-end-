dbcd28842e2c72ed2ea2a2c8e2164cd46482b84a
Sourav Mandal <sm227465@gmail.com>
2022-12-31 20:43:45 +0530

    add populate

--- db/db.js
+++ db/db.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 
 // setting path for .env file
 dotenv.config({ path: './config.env' });
-console.log(process.env.CLOUD_DB_URL);
 
 const localDB = process.env.LOCAL_DB_URL;
 const cloudDB = process.env.CLOUD_DB_URL.replace('<password>', process.env.CLOUD_DB_PASS);
@@ -15,8 +14,8 @@ const connectWithDB = () => {
     .connect(cloudDB)
     .then(() => logger.info('Connected to MongoDB.'))
     .catch((err) => {
-      logger.info('Failed to connect with MongoDB.');
-      console.log(err.message);
+      logger.error('Failed to connect with MongoDB.');
+      logger.error(err.message);
       process.exit(1);
     });
 };
